fc282c10f8c9f994e2a4cece7e3e527caa5c9c6a
Alpha Catharsis <alpha.catharsis@gmail.com>
2021-01-10 14:44:35 +0100

    bumped version to 2.0.1

--- alpha-sudo.el
+++ alpha-sudo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alpha Catharsis <alpha.catharsis@gmail.com>
 ;; Maintainer: Alpha Catharsis <alpha.catharsis@gmail.com>
-;; Version: 2.0.0
+;; Version: 2.0.1
 ;; Keywords: processes, unix
 ;; URL: https://github.com/alpha-catharsis/alpha-sudo.el
 ;; Package-Requires: ((emacs "25.1"))
